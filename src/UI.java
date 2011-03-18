6a5e42968eb5537cb8fc4583a8b15391c85b5b48
Joshua Lee <jlee02@my.fit.edu>
2011-03-18 17:14:22 -0400

    Going to attempt to use the new architecture on next commit.

--- src/UI.java
+++ src/UI.java
@@ -13,12 +13,14 @@ public class UI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Team Fireball's Degree Planner");
-		while (!loggedIn) {
+		System.out.println("Press Ctrl+c to end.");
+		while (true) {
 			login();
-		}
-		while (loggedIn) {
-			currentStudent = findStudent(Integer.parseInt(input.nextLine()));
-			System.out.println("Editing " + currentStudent.name);
+			while (loggedIn) {
+				System.out.print("Student User ID: ");
+				currentStudent = findStudent(Integer.parseInt(input.nextLine()));
+				System.out.println("Editing " + currentStudent.name);
+			}
 		}
 	}
 
@@ -30,7 +32,6 @@ public class UI {
 		System.out.print("Advisor Password: ");
 		advisorPass = input.nextLine();
 		if (Database.advisorDB[advisorID].equals(advisorPass)) {
-			System.out.print("Student User ID: ");
 			loggedIn = true;
 		}
 	}
@@ -41,7 +42,16 @@ public class UI {
 
 	private static Student findStudent(int studentID) {
 		if (tracks.find(studentID) == null)
-			tracks.studentDB.add(new Student(studentID, input.nextLine()));
+		{
+			System.out.println("Student ID " + studentID + " not found.");
+			System.out.print("Create new student profile with ID " + studentID + " (y/n)? ");
+			if (input.nextLine().equals("y"))
+			{
+				System.out.print("Student Name: ");
+				tracks.studentDB.add(new Student(studentID, input.nextLine()));
+				System.out.println("Success");
+			}
+		}
 		return tracks.find(studentID);
 	}
 }
